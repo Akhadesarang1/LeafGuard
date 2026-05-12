cf58d3cc4c75dd08be2a53c8e0ea354dc0786836
Sarang Akhade <Akhadesarang1@gmail.com>
2026-05-12 10:49:24 +0530

    doc: Add keras files along with the project

--- mern/models/User.js
+++ mern/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
-});
+}); 
 
 const User = mongoose.model("users", userSchema); // Connects to 'users' collection
 
