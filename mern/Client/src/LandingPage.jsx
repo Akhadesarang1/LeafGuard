47ef430165797738753cea3417a3f8e59ad7e083
Sarang Akhade <Akhadesarang1@gmail.com>
2026-05-28 15:15:37 +0530

    feat(fix) : fixed web Routing

--- mern/Client/src/LandingPage.jsx
+++ mern/Client/src/LandingPage.jsx
@@ -1,7 +1,7 @@
 
 import { motion } from "framer-motion";
 import { useState } from "react";
-import { Helmet } from "react-helmet"; 
+import { Helmet } from "react-helmet";
 import {
   FaBars,
   FaBrain,
